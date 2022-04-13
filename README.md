b0f267830c955bc23e1ff612df2f2d119255c65f
Connor Semb <polishmustard710@gmail.com>
2022-04-12 22:43:36 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# WeatherChannel
+# WeatherChannel Description
+
+<!-- Links -->
+
+
+
+
+<!-- Screenshot -->
+
+![Screenshot of page](./127.0.0.1_5501_index.html%20(2).png)
+
--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-// let apiKey = "eb7c6c94cbc9aa24e851279707916e63";
-
-// getWeather ();
-
-// function getWeather () {
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=portland&appid=`)
-//         .then(response => response.json())
-//         .then (weatherData => {
-//             console.log(weatherData);
-//         });
-// }
-
+// fetches weather data from the specified API key
 let weather = {
     apiKey: "eb7c6c94cbc9aa24e851279707916e63",
     fetchWeather: function (city) {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" 
-        + city 
-        + "&units=metric&appid=" 
-        + this.apiKey
-        )
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.weatherDisplay(data));
     },
-    // displayWeather: function(data) {
-        
-    // }
+    // stating variables
+    weatherDisplay: function(data) {
+        let {name} = data;
+        let {icon, description} = data.weather[0];
+        let {temp, humidity} = data.main;
+        let {speed} = data.wind;
+    // Below are query selectors that, after being fetched from above, puts the information to the corresponding elements in the html document/ webpage
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = name;
+        document.querySelector(".icon").src ="https://openweathermap.org/img/wn/"+ icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temperature").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
+    },
+    // When user inputs city, 
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
 }
+// upon user inputting specified city, then the user can click the search button to the right of the bar and the function above will retrieve information about specified city
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
+});
+// upon user inputting specified city, then the user can simply hit enter and the function above will retrieve information about specified city
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+    weather.search();
+    }
+})
+// Below pulls information on San Luis Potosi as soon as the webpage is addressed, and will change for user input
+weather.fetchWeather("San Luis Potosi");
+
